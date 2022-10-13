d5e31fd96aef49f78738e969a5802c61ef4111f3
Anderson Lomba <andersonlomba172@outlook.com>
2022-10-13 15:52:00 -0300

    Atualização JavaScript

--- src/component/HomePage.js
+++ src/component/HomePage.js
@@ -1,5 +1,4 @@
- import React from "react";
-// import React, {useState, useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import "./HomePage.css";
 import TextField from "@material-ui/core/TextField";
 import Radio from "@material-ui/core/Radio";
@@ -7,11 +6,26 @@ import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import FormControl from "@material-ui/core/FormControl";
 import FormLabel from "@material-ui/core/FormLabel";
-import Checkbox from '@material-ui/core/Checkbox';
-import Button from '@material-ui/core/Button';
-import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import Checkbox from "@material-ui/core/Checkbox";
+import Button from "@material-ui/core/Button";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 
 export default function HomePage() {
+    
+    const [Data, setData] = useState({
+        Notes: "",
+        ScheduleDate: "",
+        ScheduleTime: "",
+        OrderBy: "",
+        Segunda: false,
+        Terca: false,
+        Quarta: false,
+        Quinta: false,
+        Sexta: false,
+        Sabado: false,
+        Domingo: false,
+      });
+
   return (
     <div className="Container">
       <div className="Sub-Container">
@@ -22,6 +36,8 @@ export default function HomePage() {
             fullWidth
             size="small"
             variant="outlined"
+            value={Data.Notes}
+            onChange={setData}
           />
           <div className="Data">
             Data agendada
@@ -38,91 +54,145 @@ export default function HomePage() {
               variant="outlined"
               type="time"
             />
-            <div className="Data">
-
-            </div>
+            <div className="Data"></div>
           </div>
           <div className="Feito">
             <RadioGroup
-              
               className="RadioGroup"
-            //   value={value}
-            //   onChange={handleChange}
+              //   value={value}
+              //   onChange={handleChange}
             >
-              <FormControlLabel
-                value="ASC"
-                control={<Radio />}
-                label="Asc"
-              />
-              <FormControlLabel value="DESC" 
-              control={<Radio />} 
-              label="Desc" />
+              <FormControlLabel value="ASC" control={<Radio />} label="Asc" />
+              <FormControlLabel value="DESC" control={<Radio />} label="Desc" />
             </RadioGroup>
-            <div style={{ height:60}}>
-                <div style={{width: 350, height:22, display:"flex"}}>
-                    <div style = {{flex:1, display:'flex', justifyContent:'center'}}>Seg</div>
-                    <div style = {{flex:1, display:'flex', justifyContent:'center'}}>Ter</div>
-                    <div style = {{flex:1, display:'flex', justifyContent:'center'}}>Qua</div>
-                    <div style = {{flex:1, display:'flex', justifyContent:'center'}}>Qui</div>
-                    <div style = {{flex:1, display:'flex', justifyContent:'center'}}>Sex</div>
-                    <div style = {{flex:1, display:'flex', justifyContent:'center'}}>Sab</div>
-                    <div style = {{flex:1, display:'flex', justifyContent:'center'}}>Dom</div>
+            <div style={{ height: 60 }}>
+              <div style={{ width: 350, height: 22, display: "flex" }}>
+                <div
+                  style={{ flex: 1, display: "flex", justifyContent: "center" }}
+                >
+                  Seg
                 </div>
-                <div style={{width: 350, height:22, display:"flex"}}>
+                <div
+                  style={{ flex: 1, display: "flex", justifyContent: "center" }}
+                >
+                  Ter
+                </div>
+                <div
+                  style={{ flex: 1, display: "flex", justifyContent: "center" }}
+                >
+                  Qua
+                </div>
+                <div
+                  style={{ flex: 1, display: "flex", justifyContent: "center" }}
+                >
+                  Qui
+                </div>
+                <div
+                  style={{ flex: 1, display: "flex", justifyContent: "center" }}
+                >
+                  Sex
+                </div>
+                <div
+                  style={{ flex: 1, display: "flex", justifyContent: "center" }}
+                >
+                  Sab
+                </div>
+                <div
+                  style={{ flex: 1, display: "flex", justifyContent: "center" }}
+                >
+                  Dom
+                </div>
+              </div>
+              <div style={{ width: 350, height: 22, display: "flex" }}>
                 <Checkbox
-                 style = {{flex:1}}
-                //   checked={checked}
-                //   onChange={handleChange}                 
+                  style={{ flex: 1 }}
+                  //   checked={checked}
+                  //   onChange={handleChange}
                 />
-                     <Checkbox
-                     style = {{flex:1}}
-                //   checked={checked}
-                //   onChange={handleChange}                 
+                <Checkbox
+                  style={{ flex: 1 }}
+                  //   checked={checked}
+                  //   onChange={handleChange}
                 />
-                     <Checkbox
-                     style = {{flex:1}}
-                //   checked={checked}
-                //   onChange={handleChange}                 
+                <Checkbox
+                  style={{ flex: 1 }}
+                  //   checked={checked}
+                  //   onChange={handleChange}
                 />
-                     <Checkbox
-                     style = {{flex:1}}
-                //   checked={checked}
-                //   onChange={handleChange}                 
+                <Checkbox
+                  style={{ flex: 1 }}
+                  //   checked={checked}
+                  //   onChange={handleChange}
                 />
-                     <Checkbox
-                     style = {{flex:1}}
-                //   checked={checked}
-                //   onChange={handleChange}                 
+                <Checkbox
+                  style={{ flex: 1 }}
+                  //   checked={checked}
+                  //   onChange={handleChange}
                 />
-                     <Checkbox
-                     style = {{flex:1}}
-                //   checked={checked}
-                //   onChange={handleChange}                 
+                <Checkbox
+                  style={{ flex: 1 }}
+                  //   checked={checked}
+                  //   onChange={handleChange}
                 />
-                     <Checkbox
-                     style = {{flex:1}}
-                //   checked={checked}
-                //   onChange={handleChange}                 
-                />         
-                </div>
+                <Checkbox
+                  style={{ flex: 1 }}
+                  //   checked={checked}
+                  //   onChange={handleChange}
+                />
+              </div>
             </div>
             <div className="Button">
-            <Button variant="contained" color="primary">
-            <ThumbUpIcon/> &nbsp;
-            Feito
-         </Button>
-         </div>
-        </div>
+              <Button variant="contained" color="primary">
+                <ThumbUpIcon /> &nbsp; Feito
+              </Button>
+            </div>
+          </div>
         </div>
         <div className="Box2">
-            <div className="Data-flex" style={{margin: '7px 0 0 0', color: "blue"}}>
-            <div className="NoteId" style={{flex:1}}>Id</div>
-            <div className="Note" style={{width:300, margin:'0 30px', display: 'flex', justifyContent: 'center'}}>Anotação</div>
-            <div className="Dias" style={{flex:3, display: 'flex', justifyContent: 'center'}}>Dias</div>
-            <div className="Data-Agendada" style={{flex:2.5, display: 'flex', justifyContent: 'center'}}>Data agendada</div>
-            <div className="Hora-Agendada" style={{flex:2.5, display: 'flex', justifyContent: 'center'}}> Hora agendada</div>
-            <div className="Operacoes" style={{flex:2.5, display: 'flex', justifyContent: 'center'}}>Configuração</div>
+          <div
+            className="Data-flex"
+            style={{ margin: "7px 0 0 0", color: "blue" }}
+          >
+            <div className="NoteId" style={{ flex: 1 }}>
+              Id
             </div>
+            <div
+              className="Note"
+              style={{
+                width: 300,
+                margin: "0 30px",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              Anotação
+            </div>
+            <div
+              className="Dias"
+              style={{ flex: 3, display: "flex", justifyContent: "center" }}
+            >
+              Dias
+            </div>
+            <div
+              className="Data-Agendada"
+              style={{ flex: 2.5, display: "flex", justifyContent: "center" }}
+            >
+              Data agendada
+            </div>
+            <div
+              className="Hora-Agendada"
+              style={{ flex: 2.5, display: "flex", justifyContent: "center" }}
+            >
+              {" "}
+              Hora agendada
+            </div>
+            <div
+              className="Operacoes"
+              style={{ flex: 2.5, display: "flex", justifyContent: "center" }}
+            >
+              Configuração
+            </div>
+          </div>
         </div>
       </div>
     </div>
