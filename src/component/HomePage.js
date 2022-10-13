9dab023557ff2a971070bc65aec29c1c62b80307
Anderson Lomba <andersonlomba172@outlook.com>
2022-10-13 17:45:13 -0300

    Atualização parte 3

--- src/component/HomePage.js
+++ src/component/HomePage.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./HomePage.css";
 import TextField from "@material-ui/core/TextField";
 import Radio from "@material-ui/core/Radio";
@@ -11,56 +11,81 @@ import Button from "@material-ui/core/Button";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 
 export default function HomePage() {
+  const [Data, setData] = useState({
+    Notes: "",
+    DataAgendada: "",
+    HoraAgendada: "",
+    OrderBy: "DESC",
+    Segunda: false,
+    Terca: false,
+    Quarta: false,
+    Quinta: false,
+    Sexta: false,
+    Sabado: false,
+    Domingo: false,
+    NotesFlag: false
+  });
+
+  const handleChangeOrderBy = (event) => {
+    console.log("Order By: ", event.target.value);
     
-    const [Data, setData] = useState({
-        Notes: "",
-        ScheduleDate: "",
-        ScheduleTime: "",
-        OrderBy: "",
-        Segunda: false,
-        Terca: false,
-        Quarta: false,
-        Quinta: false,
-        Sexta: false,
-        Sabado: false,
-        Domingo: false,
-      });
+    setData({...Data,OrderBy: event.target.value});
+  };
+
+  const handleDone = () =>{
+    setData({...Data,NotesFlag: false})
+    if (Data.Notes === ""){
+        setData({...Data,NotesFlag: true})
+        console.log("Anotação é campo obrigatório");
+        return;
+    }
+
+    console.log("Data:", Data);
+  }
 
   return (
     <div className="Container">
       <div className="Sub-Container">
         <div className="Box1">
           <TextField
-            className="Note"
+          error={Data.NotesFlag}
+            className="Notes"
             label="Anotações"
             fullWidth
             size="small"
             variant="outlined"
             value={Data.Notes}
-            onChange={setData}
+            onChange={(e) => {setData({...Data, Notes:e.target.value})}}
+                
           />
           <div className="Data">
             Data agendada
             <TextField
-              className="Data-Agendada"
+              className="DataAgendada"
               size="small"
               variant="outlined"
               type="date"
+              value={Data.DataAgendada}
+              onChange={(e) => {
+                setData({ ...Data, DataAgendada: e.target.value })}}
             />
             Hora agendada
             <TextField
-              className="Hora-Agendada"
+              className="HoraAgendada"
               size="small"
               variant="outlined"
               type="time"
-            />
-            <div className="Data"></div>
+              value={Data.HoraAgendada}
+              onChange={(e) => {
+                setData({ ...Data, HoraAgendada: e.target.value })}}
+             />
+            {/* <div className="Data"></div> */}
           </div>
           <div className="Feito">
             <RadioGroup
               className="RadioGroup"
-              //   value={value}
-              //   onChange={handleChange}
+              value={Data.OrderBy}
+              onChange={handleChangeOrderBy}
             >
               <FormControlLabel value="ASC" control={<Radio />} label="Asc" />
               <FormControlLabel value="DESC" control={<Radio />} label="Desc" />
@@ -106,43 +131,43 @@ export default function HomePage() {
               <div style={{ width: 350, height: 22, display: "flex" }}>
                 <Checkbox
                   style={{ flex: 1 }}
-                  //   checked={checked}
-                  //   onChange={handleChange}
+                    checked={Data.Segunda}
+                    onChange={(e)=>{setData({...Data,Segunda:e.target.checked})}}
                 />
                 <Checkbox
                   style={{ flex: 1 }}
-                  //   checked={checked}
-                  //   onChange={handleChange}
+                  checked={Data.Terca}
+                  onChange={(e)=>{setData({...Data,Terca:e.target.checked})}}
                 />
                 <Checkbox
                   style={{ flex: 1 }}
-                  //   checked={checked}
-                  //   onChange={handleChange}
+                  checked={Data.Quarta}
+                  onChange={(e)=>{setData({...Data,Quarta:e.target.checked})}}
                 />
                 <Checkbox
                   style={{ flex: 1 }}
-                  //   checked={checked}
-                  //   onChange={handleChange}
+                  checked={Data.Quinta}
+                  onChange={(e)=>{setData({...Data,Quinta:e.target.checked})}}
                 />
                 <Checkbox
                   style={{ flex: 1 }}
-                  //   checked={checked}
-                  //   onChange={handleChange}
+                  checked={Data.Sexta}
+                  onChange={(e)=>{setData({...Data,Sexta:e.target.checked})}}
                 />
                 <Checkbox
                   style={{ flex: 1 }}
-                  //   checked={checked}
-                  //   onChange={handleChange}
+                  checked={Data.Sabado}
+                  onChange={(e)=>{setData({...Data,Sabado:e.target.checked})}}
                 />
                 <Checkbox
                   style={{ flex: 1 }}
-                  //   checked={checked}
-                  //   onChange={handleChange}
+                  checked={Data.Domingo}
+                  onChange={(e)=>{setData({...Data,Domingo:e.target.checked})}}
                 />
               </div>
             </div>
             <div className="Button">
-              <Button variant="contained" color="primary">
+              <Button variant="contained" color="primary" onClick ={()=>{handleDone()}}>
                 <ThumbUpIcon /> &nbsp; Feito
               </Button>
             </div>
@@ -157,7 +182,7 @@ export default function HomePage() {
               Id
             </div>
             <div
-              className="Note"
+              className="Notes"
               style={{
                 width: 300,
                 margin: "0 30px",
@@ -174,13 +199,13 @@ export default function HomePage() {
               Dias
             </div>
             <div
-              className="Data-Agendada"
+              className="DataAgendada"
               style={{ flex: 2.5, display: "flex", justifyContent: "center" }}
             >
               Data agendada
             </div>
             <div
-              className="Hora-Agendada"
+              className="HoraAgendada"
               style={{ flex: 2.5, display: "flex", justifyContent: "center" }}
             >
               {" "}
